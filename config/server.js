118324c4823c34944dc3aecbc0ef4843f7face24
mohammad eydi <mohammadeydi2020@gmail.com>
2026-02-26 11:12:16 +0330

    feat: add initial server configuration with host, port, URL, app keys, and admin settings.

--- config/server.js
+++ config/server.js
@@ -1,12 +1,23 @@
-// path: ./config/server.js
 
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   url: env('STRAPI_ADMIN_BACKEND_URL', 'https://tarhelahi-nodejs.liara.run'),
-  proxy: true, // مستقیم مقدار true بده تا هیچ شک و شبهه‌ای باقی نماند
+  proxy: true,
   app: {
     keys: env.array('APP_KEYS'),
   },
-  // بقیه موارد دست نخورده بماند...
+  admin: {
+    auth: {
+      events: {
+        onConnectionError(e) {
+          console.error(e);
+        },
+      },
+    },
+    forgotPassword: {
+      from: 'no-reply@tarhelahi.ir',
+      replyTo: 'support@tarhelahi.ir',
+    },
+  },
 });
