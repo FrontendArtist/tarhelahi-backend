4968ff1f795288694dad1e922c33f30390da5830
mohammad eydi <mohammadeydi2020@gmail.com>
2026-02-26 11:12:01 +0330

    feat: introduce server configuration file

--- config/server.js
+++ config/server.js
@@ -4,23 +4,9 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   url: env('STRAPI_ADMIN_BACKEND_URL', 'https://tarhelahi-nodejs.liara.run'),
-  // این خط حیاتی است: به استراپی می‌گوید که پشت پروکسی لیارا است
-  proxy: env.bool('IS_PROXIED', true), 
+  proxy: true, // مستقیم مقدار true بده تا هیچ شک و شبهه‌ای باقی نماند
   app: {
     keys: env.array('APP_KEYS'),
   },
-  admin: {
-    auth: {
-      events: {
-        onConnectionError(e) {
-          console.error(e);
-        },
-      },
-    },
-    // تنظیمات کوکی برای محیط پروداکشن
-    forgotPassword: {
-      from: 'no-reply@tarhelahi.ir',
-      replyTo: 'support@tarhelahi.ir',
-    },
-  },
+  // بقیه موارد دست نخورده بماند...
 });
